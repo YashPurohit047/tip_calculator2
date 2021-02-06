74be223bf45a7716f2cc0a28f9b0c86076441547
YashPurohit047 <f20200946@pilani.bits-pilani.ac.in>
2021-02-06 23:59:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Tip Calculator',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  double amount =0, percentagetip = 0, total =0, tipamount = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tip Calculator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Bill Amount',
+                    hintText: 'Enter Bill Amount e.g. 6000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+                onChanged: (String str){
+                  amount = double.parse(str);
+                }
+            ),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Tip Percentage',
+                    hintText: 'Enter Tip Percentage e.g. 10',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+                onChanged: (String str2){
+                  percentagetip = double.parse(str2);
+                }
+            ),
+            ElevatedButton(
+              child: Text('Calculate Total Bill'),
+              onPressed: () {
+                tipamount = (percentagetip/100)*amount;
+                total = amount + tipamount;
+                print("entered amount - $amount , tip amount - $tipamount , total amount - $total");
+                calcBill(context);
+              }
+                ),
+          ],
+        ),
+      ),
+    );
+}
+
+  void calcBill(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Bill Details"),
+      content: Text("Entered Amount - $amount \n Tip amount - $tipamount \n Total Amount - $total"),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => alertDialog);
+  }
+  }
